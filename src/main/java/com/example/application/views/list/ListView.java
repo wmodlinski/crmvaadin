69d6c3fb199b4316a1a84b0816d8b5fdeb92d055
Waldemar Modliński <103046767+wmodlinski@users.noreply.github.com>
2023-02-07 09:48:58 +0100

    Dodanie paska bocznego

--- src/main/java/com/example/application/views/list/ListView.java
+++ src/main/java/com/example/application/views/list/ListView.java
@@ -12,7 +12,7 @@ import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
-@Route(value = "")
+@Route(value="", layout = MainLayout.class)
 @PageTitle("Contacts | Vaadin CRM")
 public class ListView extends VerticalLayout {
 
