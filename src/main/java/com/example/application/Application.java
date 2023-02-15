766c482ac7e7295322918bf25cc5b67c3ce2bf36
Waldemar Modliński <103046767+wmodlinski@users.noreply.github.com>
2023-02-15 11:44:26 +0100

    Dodanie opcji "Offline"

--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -2,20 +2,16 @@ package com.example.application;
 
 import com.vaadin.flow.component.dependency.NpmPackage;
 import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * The entry point of the Spring Boot application.
- *
- * Use the @PWA annotation make the application installable on phones, tablets
- * and some desktop browsers.
- *
- */
+
 @SpringBootApplication
 @Theme(value = "flowcrmtutorial")
 @NpmPackage(value = "line-awesome", version = "1.3.0")
+@PWA(name = "VaadinCRM", shortName = "CRM", offlinePath="offline.html", offlineResources = { "./images/offline.png"})
 public class Application implements AppShellConfigurator {
 
     public static void main(String[] args) {
